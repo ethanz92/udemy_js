2466e6628b26cf62170251be233f7221e31dcd82
ethanz92 <109267902+ethanz92@users.noreply.github.com>
2022-09-02 13:26:57 +1000

    1SEP

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+/* let js = 'amazing';
+if (js=== 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun, typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun, typeof javascriptIsFun);
+
+let year;
+console.log(year, typeof year);
+year = 1991
+console.log(year, typeof year);
+
+console.log(typeof null); // returns object - this is a bug of js
+*/
+
+const birthYear = 1991;
+// birthYear = 1990;
