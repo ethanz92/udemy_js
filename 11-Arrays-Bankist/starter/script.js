168bb5ae3dbbb52a9add2d9a982d892e473cdb11
ethanz92 <109267902+ethanz92@users.noreply.github.com>
2022-10-17 21:31:29 +1100

    17 OCT

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -512,7 +512,7 @@ console.log(movements);
 
 //implementing sorting balances (refer const displayMovements)
 */
-
+/*
 // SECTION More ways to create and filling arrays
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
@@ -544,6 +544,73 @@ labelBalance.addEventListener('click', function () {
   console.log(movementsUI);
   // const movementsUI2 = [...document.querySelectorAll('.movements__value')]; //方法2，不推荐
 });
+*/
+
+/// CHAP Array methods practice
+/*
+// 1. 加总所有账户的所有交易
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2. 超过1000的数目
+//method 1
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+
+//method 2
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+// .reduce((count, cur) => (cur >= 1000 ? count ++ : count), 0);
+// ++不行，虽然++会加一，但是return的是old value
+// .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+// 这样就可以
+
+console.log(numDeposits1000);
+
+//ATTN ++
+let a = 10;
+console.log(a++); //10
+console.log(a); //11
+console.log(++a); //12
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur); //ATTN
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur; //等价于 ATTN
+      return sums; //ATTN
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalise = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalise(word)))
+    .join(' ');
+  return capitalise(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+*/
 
 /// CHAP Challenges
 ///////////////////////////////////////
@@ -635,3 +702,64 @@ const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 */
+
+////////////////////////////////////////
+// SECTION Coding Challenge #4
+// Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+// Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+// Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+// 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+dogs.forEach(dog => {
+  dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+console.log(dogs);
+
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
+const eating = function (dog) {
+  console.log(
+    `${
+      dog.curFood > dog.recommendedFood
+        ? 'Eating too much!'
+        : 'Eating too little!'
+    }`
+  );
+};
+
+const findOwner = function (owner) {
+  return dogs.filter(dog => dog.owners.includes(owner))[0];
+};
+
+const dogSarah = findOwner('Sarah');
+console.log(dogSarah);
+eating(dogSarah);
+
+// 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+// 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+// 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+// HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+// HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
